63969cfc717370fcd792c60ece584ebc9d49a086
Desi Mandasari <desimandasari1993@gmail.com>
2019-05-22 20:53:58 +0700

    Test to getting data from Giphy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+var GphApiClient = require("giphy-js-sdk-core");
+const client_key = GphApiClient("mmBoU4I5w6UfsLVutRV3bcVFIFWRl4uH");
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      data: []
+    };
   }
-  render() { 
-    return ( <h1>Hahahahaha</h1> );
+
+  searchImage() {
+    let some_data = this.state.data;
+    client_key
+      .search("gifs", { q: "cats" })
+      .then(response => {
+        response.data.forEach(gifObject => {
+          this.setState({
+            data: some_data.push(gifObject)
+          });
+          /* return (
+            <div className="col-2">
+              <img src="" alt="" />
+            </div>
+          ); */
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    this.searchImage();
+    console.log(this.state.data);
+    return (
+      <div>
+        <h2>Hello World</h2>
+        <div className="imageContainer row" />
+      </div>
+    );
   }
 }
- 
+
 export default App;
